f7385ed8ff92adac0df43492da32a58e7f43ca71
Alternate Name <alternate-email@domain>
2017-04-27 16:58:51 +0300

    Added iptraf

--- .scripts/install/various_install.sh
+++ .scripts/install/various_install.sh
@@ -2,5 +2,7 @@
 
 # ncdu - Ncurses interface du
 # htop - Improved top
+# clang - C language framework
+# iptraf - monitor IP traffic
 
-sudo apt-get -y install htop vim ncdu clang
+sudo apt-get -y install htop vim ncdu clang iptraf
