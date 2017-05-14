3174a46660a61a8329364712f087c8d020e896b6
Alternate Name <alternate-email@domain>
2017-05-15 00:44:42 +0300

    Updated generate script

--- .scripts/generate_packet.py
+++ .scripts/generate_packet.py
@@ -1,30 +1,41 @@
+#!/usr/bin/python
 """
 Generates a packet with a constant settings for a given payload
 """
-from scapy.all import *
+import sys
+from scapy.all import Ether, IP, TCP, Raw, wrpcap
 
 PCAP_NAME = 'a.pcap'
 SRC_MAC = "00:50:56:b3:c7:fb"
 DST_MAC = "00:50:56:b3:c7:fc"
 SRC_IP = "4.4.4.4"
 DST_IP = "4.4.4.5"
-SRC_PORT = 12345
-DST_PORT = 1010
+RANDOM_PORT = 12345
+MODBUS_PORT = 1010
+
+def build_pcap(payload, src_port, dst_port):
+    """
+    Building a pcap from the given payload and the ports
+    """
+    # Build all the layers
+    ethernet_layer = Ether(dst=SRC_MAC, src=DST_MAC)
+    ip_layer = IP(src=SRC_IP, dst=DST_IP)
+    tcp_layer = TCP(sport=src_port, dport=dst_port)
+    payload = Raw(load=bytearray.fromhex(payload))
+
+    complete_packet = ethernet_layer/ip_layer/tcp_layer/payload
+
+    wrpcap(PCAP_NAME, complete_packet)
 
 
 def main():
     """
     Main function
     """
-    # Build all the layers
-    ethernet_layer = Ether(dst=SRC_MAC, src=DST_MAC)
-    ip_layer = IP(src=SRC_IP, dst=DST_IP)
-    tcp_layer = TCP(sport=DST_PORT, dport=1010)
-    payload = Raw(load=sys.argv[1])
-
-    complete_packet = ethernet_layer/ip_layer/tcp_layer/payload
-
-    wrpcap(PCAP_NAME, complete_packet)
+    if sys.argv[1] == '-r':
+        build_pcap(sys.argv[2], MODBUS_PORT, RANDOM_PORT)
+    else:
+        build_pcap(sys.argv[1], RANDOM_PORT, MODBUS_PORT)
 
 if __name__ == "__main__":
     main()
