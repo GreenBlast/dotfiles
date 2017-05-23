6be2cd7cea666b7bc1f559be34209ed425fad034
Alternate Name <alternate-email@domain>
2017-05-23 11:02:20 +0300

    Added xsel, tmux copycat and TODOs

--- .scripts/install/various_install.sh
+++ .scripts/install/various_install.sh
@@ -7,6 +7,7 @@
 # exuberant-ctags - Ctags for C language
 # ipython - Python shell
 # xclip - Clipboard utility
+# xsel - Clipboard utility
 
 
-sudo apt-get -y install htop vim ncdu clang iptraf exuberant-ctags ipython xclip
+sudo apt-get -y install htop vim ncdu clang iptraf exuberant-ctags ipython xclip xsel
