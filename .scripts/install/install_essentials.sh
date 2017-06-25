0bcb607b84db36198289e1b640543fb4e8cd2718
Alternate Name <alternate-email@domain>
2017-06-25 18:53:39 +0300

    Tried to change order of installations to be more robust

--- .scripts/install/install_essentials.sh
+++ .scripts/install/install_essentials.sh
@@ -13,8 +13,9 @@ echo "Updating"
 $RUN_AS_ROOT apt-get update
 
 echo "Installing"
+$RUN_AS_ROOT apt-get -y install zsh tmux python-dev python-pip python3-dev python3-pip silversearcher-ag
 # Installing neovim
-$RUN_AS_ROOT apt-get -y install zsh tmux python-dev python-pip python3-dev python3-pip silversearcher-ag neovim
+$RUN_AS_ROOT apt-get -y install neovim
 
 echo "Pip installing"
 pip2 install --user neovim
