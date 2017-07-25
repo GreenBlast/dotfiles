65856f9d383a631f9461220267c2a715374a9ba1
Alternate Name <alternate-email@domain>
2017-07-25 17:24:48 +0300

    Finished nvim install script not tested yet

--- .scripts/install/neovim_download_build_install.sh
+++ .scripts/install/neovim_download_build_install.sh
@@ -7,12 +7,18 @@ else
 fi
 
 echo "Install packages for building"
-RUN_AS_ROOT apt -y install git libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
+$RUN_AS_ROOT apt -y install git libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
 
 echo "Git clone nevim"
-
+mkdir -p $HOME/neovim_build/
+cd $HOME/neovim_build/
+git clone https://github.com/neovim/neovim.git
 
 echo "Building neovim, this should take a lot of time"
+$RUN_AS_ROOT make CMAKE_BUILD_TYPE=RelWithDebInfo
+$RUN_AS_ROOT make install
+
 
 echo "Installing neovim"
-
+$RUN_AS_ROOT cp $HOME/neovim_build/neovim/build/bin/nvim /usr/bin/
+chmod +x $HOME/neovim_build/neovim/build/bin/nvim /usr/bin/
