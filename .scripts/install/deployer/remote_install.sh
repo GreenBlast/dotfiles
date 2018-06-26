0176acdb89908b4dc899f69fbc5a987c4cf6045d
Alternate Name <alternate-email@domain>
2018-06-26 17:01:26 +0300

    Updated remote install script

--- .scripts/install/deployer/remote_install.sh
+++ .scripts/install/deployer/remote_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: ${0} remote_machine(ssh format)"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: ${0} username address"
     exit 1
 fi
 
-scp -r $HOME/.scripts/install/deployer $1:~/
-ssh $1 ~/deployer/yadm_start.sh
+scp -r $HOME/.scripts/install/deployer $1@$2:/home/$1/
+ssh $1@$2 /home/$1/deployer/yadm_start.sh
