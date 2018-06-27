32477e92dd6e4dbb015bd19632b15dacb8234ab5
Alternate Name <alternate-email@domain>
2018-06-27 17:59:19 +0300

    Updated yadm start script

--- .scripts/install/deployer/yadm_start.sh
+++ .scripts/install/deployer/yadm_start.sh
@@ -12,13 +12,24 @@ type git || {
 }
 echo ""
 
+# Check wget command
+type wget || {
+    echo "No wget, Attempting to install wget"
+    apt-get install -q -y wget
+
+    type wget || {
+        echo "Wget install failed, exiting"
+        exit 1
+    }
+}
+echo ""
 
 # Check yadm command
 
 type yadm || {
 echo 'Attempting to download yadm'
 mkdir -p $HOME/.local/bin/
-curl -fLo $HOME/.local/bin/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm || exit 1
+wget -O $HOME/.local/bin/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm || exit 1
 chmod a+x $HOME/.local/bin/yadm || exit 1
 
 }
