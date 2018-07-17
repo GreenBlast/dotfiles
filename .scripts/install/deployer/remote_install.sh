e011639ad3cd9380a29fdcfa71ceb1a355ee8d49
Alternate Name <alternate-email@domain>
2018-07-17 15:48:27 +0300

    Updated paths

--- .scripts/install/deployer/remote_install.sh
+++ .scripts/install/deployer/remote_install.sh
@@ -5,5 +5,5 @@ if [ "$#" -ne 2 ]; then
     exit 1
 fi
 
-scp -r $HOME/.scripts/install/deployer $1@$2:/home/$1/
+scp -r $HOME/.scripts/install/deployer $1@$2:/home/$1/deployer
 ssh $1@$2 /home/$1/deployer/yadm_start.sh
