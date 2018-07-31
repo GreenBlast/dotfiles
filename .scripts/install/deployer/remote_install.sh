74731869b1c7a4189ddc38c93ec407e6138efb00
Alternate Name <alternate-email@domain>
2018-07-31 18:51:12 +0300

    Added root behaviour for remote install

--- .scripts/install/deployer/remote_install.sh
+++ .scripts/install/deployer/remote_install.sh
@@ -5,5 +5,11 @@ if [ "$#" -ne 2 ]; then
     exit 1
 fi
 
-scp -r $HOME/.scripts/install/deployer $1@$2:/home/$1/deployer
-ssh $1@$2 /home/$1/deployer/yadm_start.sh
+if [ "$1" == "root" ]; then
+    scp -r $HOME/.scripts/install/deployer $1@$2:/$1/deployer
+    ssh $1@$2 /$1/deployer/yadm_start.sh
+else
+    scp -r $HOME/.scripts/install/deployer $1@$2:/home/$1/deployer
+    ssh $1@$2 /home/$1/deployer/yadm_start.sh
+fi
+
