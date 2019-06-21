c5284188bd496bbbb17116169b05354e06f56034
Aviad Coppenhagen <aviad@steps.me>
2019-06-21 11:06:42 +0300

    Fixed vcsh setup script

--- .scripts/install/setup_vcsh.sh
+++ .scripts/install/setup_vcsh.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd $HOME
-vcsh encrypt clone git@bitbucket.org:greenfighter/encrypt.git
+vcsh clone git@bitbucket.org:greenfighter/encrypt.git encrypt
 vcsh encrypt config user.name "GreenBlast"
 vcsh encrypt config user.email "greenfighter@gmail.com"
 vcsh encrypt config --local status.showUntrackedFiles no
