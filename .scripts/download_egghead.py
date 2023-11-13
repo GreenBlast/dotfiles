3bc5d63fccbe0a2fec862d017ac976e559412f60
Aviad Coppenhagen <aviad@steps.me>
2023-11-13 19:20:46 +0200

    Improving egghead script

--- .scripts/download_egghead.py
+++ .scripts/download_egghead.py
@@ -12,11 +12,53 @@ lesson_prefix = 'https://egghead.io'
 
 filepath = '.'
 
-def download_matches(matches):
+def download_item(item):
+    url_input = f'{lesson_prefix}{item}'
+    # process = subprocess.run([f'/usr/local/bin/yt-dlp', url_input], env={'LANG': 'en_US.UTF-8'},)
 
-    for item in matches:
-        url_input = f'{lesson_prefix}{item}'
-        process = subprocess.run([f'/usr/local/bin/yt-dlp', url_input], env={'LANG': 'en_US.UTF-8'},)
+    # if process.returncode == 0:
+    #     # The download was successful
+    #     # Parse the filename from the standard output
+    #     lines = stdout.decode('utf-8').split('\n')
+    #     for line in lines:
+    #         if line.startswith('[download] Destination: '):
+    #             downloaded_file = line.replace('[download] Destination: ', '').strip()
+    #             downloaded_files.append(downloaded_file)
+    # else:
+    #     # Handle download error
+    #     print(f"Error downloading {url_input}: {stderr.decode('utf-8')}")
+
+    # print(downloaded_files)
+    process = subprocess.Popen([f'/usr/local/bin/yt-dlp', '--print', 'filename', url_input], stdout=subprocess.PIPE, stderr=subprocess.PIPE, env={'LANG': 'en_US.UTF-8'})
+    stdout, stderr = process.communicate()
+
+    if process.returncode == 0:
+        # The download was successful
+        # Parse the filename from the standard output
+        lines = stdout.decode('utf-8').split('\n')
+        downloaded_file = ''.join(lines).strip()
+        print(f"Downloaded: {downloaded_file}")
+    else:
+        # Handle download error
+        print(f"Error downloading {url}: {stderr.decode('utf-8')}")
+
+    process = subprocess.run([f'/usr/local/bin/yt-dlp', url_input], env={'LANG': 'en_US.UTF-8'},)
+
+    return downloaded_file
+
+def rename_item(filepath_to_dir, filename, i):
+    new_name = f"{i:05} - {filename}"
+    old_file = os.path.join(filepath_to_dir, filename)
+    new_file = os.path.join(filepath_to_dir, new_name)
+    os.rename(old_file, new_file)
+    print (f"[+] - New file name: \"{new_name}\"")
+
+
+
+def download_matches_and_rename(matches, filepath_to_dir):
+    for i, item in enumerate(matches):
+        downloaded_file = download_item(item)
+        rename_item(filepath_to_dir, downloaded_file, i + 1)
 
 
 def get_url_data(url):
@@ -64,7 +106,10 @@ if __name__ == "__main__":
     else:
         url = sys.argv[1]
         matches = get_url_data(url)
-        download_matches(matches)
-        rename_files_in_dir(filepath)
+        # matches = ['/lessons/astro-intro-build-a-full-stack-blog-with-astro']
+        # download_matches_and_rename(matches, filepath)
+        # Last link is video link itself
+        download_matches_and_rename(matches[:-1], filepath)
+        # rename_files_in_dir(filepath)
 
 
