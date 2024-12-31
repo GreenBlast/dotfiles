c3b4174438c85e97a73df4a16628c34b8cd82f09
Aviad Coppenhagen <aviad@steps.me>
2024-12-31 21:59:17 +0200

    Upgrading lazyvim packages

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -66,23 +66,24 @@ return {
         harpoon:list():next()
       end)
 
-      local conf = require("telescope.config").values
+      -- Couldn't find telescope config after an upgrade
+      -- local conf = require("telescope.config").values
       local function toggle_telescope(harpoon_files)
         local file_paths = {}
         for _, item in ipairs(harpoon_files.items) do
           table.insert(file_paths, item.value)
         end
 
-        require("telescope.pickers")
-          .new({}, {
-            prompt_title = "Harpoon",
-            finder = require("telescope.finders").new_table({
-              results = file_paths,
-            }),
-            previewer = conf.file_previewer({}),
-            sorter = conf.generic_sorter({}),
-          })
-          :find()
+        -- require("telescope.pickers")
+        --   .new({}, {
+        --     prompt_title = "Harpoon",
+        --     finder = require("telescope.finders").new_table({
+        --       results = file_paths,
+        --     }),
+        --     previewer = conf.file_previewer({}),
+        --     sorter = conf.generic_sorter({}),
+        --   })
+        --   :find()
       end
 
       vim.keymap.set("n", "<C-e>", function()
